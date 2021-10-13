36ab4c5124f04f13bbb178374412c8bf92d58eb6
leny133 <puertorican.dev@gmail.com>
2021-10-13 07:02:33 -0400

    fix pre commit suggestions

--- una_hora/search/apps.py
+++ una_hora/search/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class SearchConfig(AppConfig):
-    name = 'una_hora.search'
+    name = "una_hora.search"
--- una_hora/search/views.py
+++ una_hora/search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def search_page(request):
     print("hola")
-    return render(request,"./search/search_page.html")
+    return render(request, "./search/search_page.html")
